caa25f424537ab23ec99acd4e1f92ac90101dac6
Josanu Andrei <andrei@quadra.md>
2024-04-09 10:23:43 +0300

    Add app bootstrap files

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+require dirname(__DIR__) . "/api-app/vendor/autoload.php";
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+$id = $parts[3] ?? null;
+
+
+// create response codes
+if($resource != 'tasks') {
+//    header("HTTP/1.1 404 Not Found");
+    http_response_code(404); // recommended way to use response codes
+    exit;
+}
+
+$controller = new TaskController;
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
