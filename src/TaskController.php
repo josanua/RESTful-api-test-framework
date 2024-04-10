13f67ca140ab5f2cada99808b81f4cbaa1fe42f3
Josanu Andrei <andrei@quadra.md>
2024-04-10 11:01:32 +0300

    Add MethodNotAllowed() for header and response codes

--- src/TaskController.php
+++ src/TaskController.php
@@ -2,13 +2,16 @@
 
 class TaskController
 {
-    public function processRequest(string $method, ?string $id) : void
+    public function processRequest(string $method, ?string $id): void
     {
         if ($id === null) {
+
             if ($method == "GET") {
                 echo "index";
             } elseif ($method == "POST") {
                 echo "create";
+            } else {
+                $this->respondMethodNotAllowed("GET, POST");
             }
         } else {
             switch ($method) {
@@ -23,7 +26,16 @@ class TaskController
                 case "DELETE":
                     echo "delete $id";
                     break;
+
+                default:
+                    $this->respondMethodNotAllowed("GET, PATCH, DELETE");
             }
         }
+    }
+
+    private function respondMethodNotAllowed(string $allowed_methods): void
+    {
+        http_response_code(405);
+        header("Allow: $allowed_methods");
     }
 }
