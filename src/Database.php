228c2e0f47c08d4ef06f86f13d7b06f3366f771b
Josanu Andrei <andrei@quadra.md>
2024-05-10 12:08:56 +0300

    Create class code and Connection method with PDO

--- src/Database.php
+++ src/Database.php
@@ -2,5 +2,28 @@
 
 class Database
 {
+    private  $host;
+    private  $name;
+    private  $user;
+    private  $password;
 
+    public function __construct(
+        string $host,
+        string $name,
+        string $user,
+        string $password
+    ) {
+        $this->password = $password;
+        $this->user = $user;
+        $this->name = $name;
+        $this->host = $host;
+    }
+
+    public function getConnection(): PDO {
+        $dsn = "mysql:host=$this->host;dbname=$this->name";
+
+        return PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
 }
