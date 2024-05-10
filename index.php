243813d7e899e2cc0141105049630b3891f8b14b
Josanu Andrei <andrei@quadra.md>
2024-05-10 12:02:31 +0300

    Add Database fileClass

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
 
-require dirname(__DIR__) . "/rest-api-test/vendor/autoload.php";
+require dirname(__DIR__) . "/api/vendor/autoload.php";
+
+// error handler
 set_exception_handler("\\ErrorHandler::handleException");
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
@@ -11,12 +13,14 @@ $resource = $parts[2];
 $id = $parts[3] ?? null;
 
 // create response codes
-if($resource != 'tasks') {
-//    header("HTTP/1.1 404 Not Found");
+if ($resource != 'tasks') {
+    // header("HTTP/1.1 404 Not Found");
     http_response_code(404); // recommended way to use response codes
     exit;
 }
 
-header("Content-Type: application/json; charset=UTF-8" );
+// set to return JSON Content Type
+header("Content-Type: application/json; charset=UTF-8");
+
 $controller = new TaskController;
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
