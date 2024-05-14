07fb483d44ee2bbc3b880c3159462289303a4647
Josanu Andrei <andrei@quadra.md>
2024-05-14 09:47:18 +0300

    Develop TaskGateway to fit our needs

--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -3,8 +3,19 @@
 class TaskGateway
 {
     private PDO $conn;
+
     public function __construct(Database $db)
     {
         $this->conn = $db->getConnection();
+    }
+
+    public function getAllTasksList(): array {
+        $sql = "SELECT * FROM task ORDER BY name";
+
+        // will return a PDO statement object
+        $stmt = $this->conn->query($sql);
+
+        // call fetchAll to get an array of all methods
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
