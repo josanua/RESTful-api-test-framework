b9506486982b02124016956d74fc930e2eee88d7
Josanu Andrei <andrei@quadra.md>
2024-05-17 11:32:07 +0300

    Improve to work with not found ID's values, create respondNotFound() method

--- src/TaskController.php
+++ src/TaskController.php
@@ -21,10 +21,18 @@ class TaskController
                 $this->respondMethodNotAllowed("GET, POST");
             }
         } else {
+
+            $task = $this->gateway->get($id);
+
+            if ($task === false) {
+                $this->respondNotFound($id);
+                return;
+            }
+
             switch ($method) {
 
                 case "GET":
-                    echo json_encode($this->gateway->get($id));
+                    echo json_encode($task);
                     break;
 
                 case "PATCH":
@@ -45,5 +53,10 @@ class TaskController
     {
         http_response_code(405);
         header("Allow: $allowed_methods");
+    }
+
+    private function respondNotFound(string $id): void {
+        http_response_code(404);
+        echo json_encode(["message" => "Task with ID = $id not found"]);
     }
 }
