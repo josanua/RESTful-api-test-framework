d68ce2c6c9d326ee2c7ee2929822a32abe1c9d05
Josanu Andrei <andrei@quadra.md>
2024-05-31 17:42:38 +0300

    Add register.php, connect autoload and create DB connection

---
+++ register.php
@@ -0,0 +1,58 @@
+
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $database = new Database(
+            $_ENV['DB_HOST'],
+            $_ENV['DB_NAME'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASS']
+    );
+
+    $conn = $database->getConnection();
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+</head>
+<body>
+<main class="container">
+    <h1>Register</h1>
+
+    <form method="post">
+        <label for="name">
+            Name
+            <input type="text" name="name" id="name">
+        </label>
+
+        <label for="username">
+            Username
+            <input type="text" name="username" id="username">
+        </label>
+
+        <label for="password">
+            Password
+            <input type="password" name="password" id="password">
+        </label>
+
+        <button>Register</button>
+    </form>
+</main>
+</body>
+</html>
+
+<?php
