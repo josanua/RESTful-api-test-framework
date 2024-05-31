bc7adc1f235a79e163215c6455c1e38d0b559ea6
Josanu Andrei <andrei@quadra.md>
2024-05-31 14:52:23 +0300

    Update all TaskGateway class file

--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -4,28 +4,24 @@ class TaskGateway
 {
     private PDO $conn;
 
-    public function __construct(Database $db)
+    public function __construct(Database $database)
     {
-        $this->conn = $db->getConnection();
+        $this->conn = $database->getConnection();
     }
 
-    public function getAllTasksList(): array
+    public function getAll(): array
     {
-        $sql = "SELECT * FROM task ORDER BY name";
+        $sql = "SELECT *
+                FROM task
+                ORDER BY name";
 
-        // will return a PDO statement object
         $stmt = $this->conn->query($sql);
 
-        // call fetchAll to get an array of all methods
-        // return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        // transform int to bool
         $data = [];
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
-            // casting int val. to boolean
-            $row['is_completed'] = (bool)$row['is_completed'];
+            $row['is_completed'] = (bool) $row['is_completed'];
 
             $data[] = $row;
         }
@@ -33,14 +29,14 @@ class TaskGateway
         return $data;
     }
 
-    public function get(string $id): array|false
+    public function get(string $id): array | false
     {
-        $sql = "SELECT * FROM task WHERE id = :id";
+        $sql = "SELECT *
+                FROM task
+                WHERE id = :id";
 
-        // use prepare() to avoid SQL injections.
         $stmt = $this->conn->prepare($sql);
 
-        // bind value as int type
         $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
         $stmt->execute();
@@ -48,37 +44,80 @@ class TaskGateway
         $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($data !== false) {
-            // casting int val. to boolean
-            $data['is_completed'] = (bool)$data['is_completed'];
+
+            $data['is_completed'] = (bool) $data['is_completed'];
         }
 
-        // return data will contain an array or false value
         return $data;
     }
 
     public function create(array $data): string
     {
-        $sql = "INSERT INTO task (name, priority, is_completed) 
+        $sql = "INSERT INTO task (name, priority, is_completed)
                 VALUES (:name, :priority, :is_completed)";
 
-        // prepare method
         $stmt = $this->conn->prepare($sql);
 
-        // bind values
-        $stmt->bindValue(":name", $data['name'], PDO::PARAM_STR);
+        $stmt->bindValue(":name", $data["name"], PDO::PARAM_STR);
 
-        if (empty($data['priority'])) {
+        if (empty($data["priority"])) {
+
             $stmt->bindValue(":priority", null, PDO::PARAM_NULL);
+
         } else {
-            $stmt->bindValue(":priority", $data['priority'], PDO::PARAM_INT);
+
+            $stmt->bindValue(":priority", $data["priority"], PDO::PARAM_INT);
+
         }
 
-        $stmt->bindValue(":is_completed", $data['is_completed'] ?? false, PDO::PARAM_BOOL);
+        $stmt->bindValue(":is_completed", $data["is_completed"] ?? false,
+            PDO::PARAM_BOOL);
 
-        // execute
         $stmt->execute();
 
-        // return last created id
         return $this->conn->lastInsertId();
     }
+
+    public function update(string $id, array $data)
+    {
+        $fields = [];
+
+        if ( ! empty($data["name"])) {
+
+            $fields["name"] = [
+                $data["name"],
+                PDO::PARAM_STR
+            ];
+        }
+
+        if (array_key_exists("priority", $data)) {
+
+            $fields["priority"] = [
+                $data["priority"],
+                $data["priority"] === null ? PDO::PARAM_NULL : PDO::PARAM_INT
+            ];
+        }
+
+        if (array_key_exists("is_completed", $data)) {
+
+            $fields["is_completed"] = [
+                $data["is_completed"],
+                PDO::PARAM_BOOL
+            ];
+        }
+
+        print_r($fields);
+        exit;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
