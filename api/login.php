89fd1f1e42f49760359bddb72d9327e435576a84
Josanu Andrei <andrei@quadra.md>
2024-06-11 11:48:21 +0300

    Add necessary thing for creating Auth with access tokens

--- api/login.php
+++ api/login.php
@@ -5,23 +5,32 @@ declare(strict_types=1);
 require __DIR__ . "/bootstrap.php";
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-
+    
     http_response_code(405);
     header("Allow: POST");
     exit;
 }
 
-$data = (array)json_decode(file_get_contents("php://input"), true);
+$data = (array) json_decode(file_get_contents("php://input"), true);
 
-if (!array_key_exists("username", $data) ||
-    !array_key_exists("password", $data)) {
+if ( ! array_key_exists("username", $data) ||
+     ! array_key_exists("password", $data)) {
 
     http_response_code(400);
     echo json_encode(["message" => "missing login credentials"]);
     exit;
 }
 
-echo json_encode($data);
+$database = new Database($_ENV["DB_HOST"],
+                         $_ENV["DB_NAME"],
+                         $_ENV["DB_USER"],
+                         $_ENV["DB_PASS"]);
+                         
+$user_gateway = new UserGateway($database);
+
+$user = $user_gateway->getByUsername($data["username"]);
+
+echo json_encode($user);
 
 
 
