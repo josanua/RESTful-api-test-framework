c8b03bec3d5ea3efd5ba53b31763599126ccaa3e
Josanu Andrei <andrei@quadra.md>
2024-06-12 10:47:21 +0300

    Last necessary file changes

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -5,7 +5,6 @@ require dirname(__DIR__) . "/vendor/autoload.php";
 set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
 
-//$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) . "/api/");
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
--- api/login.php
+++ api/login.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 require __DIR__ . "/bootstrap.php";
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-
+    
     http_response_code(405);
     header("Allow: POST");
     exit;
 }
 
-$data = (array)json_decode(file_get_contents("php://input"), true);
+$data = (array) json_decode(file_get_contents("php://input"), true);
 
-if (!array_key_exists("username", $data) ||
-    !array_key_exists("password", $data)) {
+if ( ! array_key_exists("username", $data) ||
+     ! array_key_exists("password", $data)) {
 
     http_response_code(400);
     echo json_encode(["message" => "missing login credentials"]);
@@ -22,23 +22,23 @@ if (!array_key_exists("username", $data) ||
 }
 
 $database = new Database($_ENV["DB_HOST"],
-    $_ENV["DB_NAME"],
-    $_ENV["DB_USER"],
-    $_ENV["DB_PASS"]);
-
+                         $_ENV["DB_NAME"],
+                         $_ENV["DB_USER"],
+                         $_ENV["DB_PASS"]);
+                         
 $user_gateway = new UserGateway($database);
 
 $user = $user_gateway->getByUsername($data["username"]);
 
 if ($user === false) {
-
+    
     http_response_code(401);
     echo json_encode(["message" => "invalid authentication"]);
     exit;
 }
 
-if (!password_verify($data["password"], $user["password_hash"])) {
-
+if ( ! password_verify($data["password"], $user["password_hash"])) {
+    
     http_response_code(401);
     echo json_encode(["message" => "invalid authentication"]);
     exit;
@@ -46,13 +46,13 @@ if (!password_verify($data["password"], $user["password_hash"])) {
 
 $payload = [
     "id" => $user["id"],
-    "name" => $user["name"],
+    "name" => $user["name"]
 ];
 
 $access_token = base64_encode(json_encode($payload));
 
 echo json_encode([
-    "access_token" => $access_token,
+    "access_token" => $access_token
 ]);
 
 
